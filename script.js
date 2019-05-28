31b305324e0017e57f1de077fa6e4292741733ec
Emma Elltorp <emma_elltorp@laptop-a9ckc8s9.kea.local>
2019-05-28 16:41:31 +0200

    js för att ladda upp dokument

--- script.js
+++ script.js
@@ -21,3 +21,39 @@ function toggleMenu() {
         document.querySelector("header .menu_toggle").textContent = "☰";
     }
 }
+
+
+//UPLOAD FILES IN AFLEVERINGAR
+// Define processing URL and form element
+const url = 'process.php'
+const form = document.querySelector('form')
+
+// Listen for form submit
+form.addEventListener('submit', e => {
+    e.preventDefault()
+
+    // ...
+})
+
+
+// Gather files and begin FormData
+const files = document.querySelector('[type=file]').files;
+const formData = new FormData();
+});
+
+// ...
+// Append files to files array
+for (let i = 0; i < files.length; i++) {
+    let file = files[i]
+
+    formData.append('files[]', file)
+}
+
+// ...
+
+fetch(url, {
+    method: 'POST',
+    body: formData,
+}).then(response => {
+    console.log(response)
+})
