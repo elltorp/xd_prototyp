3bf5a85a24855be53c158a016bab8cae95ee1228
Emma Elltorp <emma_elltorp@msi.kea.local>
2019-05-29 13:41:31 +0200

    finjusteringar och aflevringsapp ok

--- script.js
+++ script.js
@@ -28,63 +28,29 @@ function toggleMenu() {
 //collaps på mina avleveringar
 //www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
 
-
+//coll är något jag sätter själv för att komma ihåg ordet "collapsible"
 var coll = document.getElementsByClassName("collapsible");
 var i;
 
-// jag har tre element som ska rullas ut, första uppgiften är nr0 dvs att dokument 3 är nr2. (0,1,2) i är valfri bokstav och kan sättas till vad som helst. Raden nedan säger: om "i är mindre än hela längden "length av innehåll, i detta fall 3 uppgifter, så forsätter den kalla på nästa linje som är en klickfunktion.
+// jag har tre element som ska rullas ut, första uppgiften är nr0 dvs att dokument 3 är nr2. (0,1,2) i är valfri bokstav och kan sättas till vad som helst. Raden nedan säger: om "i är mindre än hela längden "length av innehåll, i detta fall 3 uppgifter, så forsätter den kalla på nästa linje som är en klickfunktion. i++ betyder att den kommer att fortsätta collapsen tills det inte finns fler dokument att visa.
 for (i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
         this.classList.toggle("active");
-        //när nästa "syskonelement" blir kallat, det är divs som ligger under varandra i samma klass/div så  säger den...
+
+        //när nästa "syskonelement" blir kallat, det är divs som ligger under varandra i samma klass/div..
         var content = this.nextElementSibling;
-        //.. om mitt intehåll  inuti  .content är blockerad kommer den att visa den som none ist och ta blockeringen bort.
+
+        // ..så betyder det...
+
+        //när sidan är loadad så visar content sig som defaultinställningen Block som betyder att innehållet VISAS ( men här är det en förinställning i min css så att diaplayen faktiskt visar att den är på none, iKKE VISES.
         if (content.style.display === "block") {
             content.style.display = "none";
-            //annars så kommer den att blockera .content med alla uppgifter så att man ej kan se avleveringarna som ligger i .content eftersom att collapsen och min "i" redan har kallat på alla mina uppgifter genom första steget där i bestämmer att den ska kalla på nästa uppgift till hela lenghten är kallad på.
+
+            //annars så kommer den att VISA (block) .content med alla uppgifter så att man ej kan se avleveringarna som ligger i .content eftersom att collapsen och min "i" redan har kallat på alla mina uppgifter genom första steget där i bestämmer att den ska kalla på nästa uppgift till hela lenghten är kallad på.
         } else {
             content.style.display = "block";
         }
     });
 }
 
-////SLUT PÅ MENY
-
-
-
-/*
-UPLOAD FILES IN AFLEVERINGAR MÅSTE VI HA?
-// Define processing URL and form element
-const url = 'process.php'
-const form = document.querySelector('form')
-
-// Listen for form submit
-form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    // ...
-})
-
-
-// Gather files and begin FormData
-const files = document.querySelector('[type=file]').files;
-const formData = new FormData();
-});
-
-// ...
-// Append files to files array
-for (let i = 0; i < files.length; i++) {
-    let file = files[i]
-
-    formData.append('files[]', file)
-}
-
-// ...
-
-fetch(url, {
-    method: 'POST',
-    body: formData,
-}).then(response => {
-    console.log(response)
-})
-*/
+//slut collaps
