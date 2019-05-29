3d390ce66891eb3b0abdad93a0cf5a848d7f5e36
Emma Elltorp <emma_elltorp@msi.kea.local>
2019-05-29 09:27:05 +0200

    collaps-funktionen  i afleveringar OK!

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+////MENY
+
 window.addEventListener("load", sidenVises);
 
 function sidenVises() {
@@ -23,7 +25,35 @@ function toggleMenu() {
 }
 
 
-//UPLOAD FILES IN AFLEVERINGAR
+//collaps på mina avleveringar
+//www.w3schools.com/howto/tryit.asp?filename=tryhow_js_collapsible
+
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+// jag har tre element som ska rullas ut, första uppgiften är nr0 dvs att dokument 3 är nr2. (0,1,2) i är valfri bokstav och kan sättas till vad som helst. Raden nedan säger: om "i är mindre än hela längden "length av innehåll, i detta fall 3 uppgifter, så forsätter den kalla på nästa linje som är en klickfunktion.
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        //när nästa "syskonelement" blir kallat, det är divs som ligger under varandra i samma klass/div så  säger den...
+        var content = this.nextElementSibling;
+        //.. om mitt intehåll  inuti  .content är blockerad kommer den att visa den som none ist och ta blockeringen bort.
+        if (content.style.display === "block") {
+            content.style.display = "none";
+            //annars så kommer den att blockera .content med alla uppgifter så att man ej kan se avleveringarna som ligger i .content eftersom att collapsen och min "i" redan har kallat på alla mina uppgifter genom första steget där i bestämmer att den ska kalla på nästa uppgift till hela lenghten är kallad på.
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
+
+////SLUT PÅ MENY
+
+
+
+/*
+UPLOAD FILES IN AFLEVERINGAR MÅSTE VI HA?
 // Define processing URL and form element
 const url = 'process.php'
 const form = document.querySelector('form')
@@ -57,3 +87,4 @@ fetch(url, {
 }).then(response => {
     console.log(response)
 })
+*/
